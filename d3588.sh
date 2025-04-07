5e3b6d11ce22a0436a9042a48d001ee75b379fb8
yifengyou <842056007@qq.com>
2025-04-07 18:45:50 +0800

    can't build in rk3588 remove useless copy
    Signed-off-by: yifengyou <842056007@qq.com>

--- d3588.sh
+++ d3588.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-set -xe
+set -e
 
-# CROSS_COMPILE_ARM64=~/toolchains/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+ARCH=`uname -m`
 
-GCC=`realpath ../gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu`
-CROSS_COMPILE_ARM64=${GCC}/bin/aarch64-none-linux-gnu-
-echo "using gcc: [${CROSS_COMPILE_ARM64}]"
+if [ X"${ARCH}" == X"x86_64" ] ; then
+	GCC=`realpath ../gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu`
+	CROSS_COMPILE_ARM64=${GCC}/bin/aarch64-none-linux-gnu-
+else
+	echo "${ARCH} is not supported now!"
+	exit 1
+fi
 
 # Clean the old files.
 rm -rf spl/u-boot-spl* #tpl/
@@ -14,9 +18,6 @@ rm -rf spl/u-boot-spl* #tpl/
 # Start building (U-Boot, SPL, etc.)
 make CROSS_COMPILE=${CROSS_COMPILE_ARM64} rk3588_defconfig
 make CROSS_COMPILE=${CROSS_COMPILE_ARM64} -j`nproc`
-
-# Copy files.
-cp spl/u-boot-spl.bin ../rkbin/bin/rk35/rk3588_spl_v3.bin
 
 # Call the official build.
 ./make.sh rk3588
@@ -27,5 +28,5 @@ mkdir -p ../rockdev/
 cp -a fit/uboot.itb ../rockdev/uboot.img
 ls -alh ../rockdev/uboot.img
 md5sum ../rockdev/uboot.img
-echo "All done! [$?]"
+echo "All done!"
 
