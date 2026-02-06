d704fed4053c8bd319078f5d2a2b5909b008b2b1
yifengyou <842056007@qq.com>
2026-02-06 02:43:03 +0000

    update build script
    Signed-off-by: yifengyou <842056007@qq.com>

--- d3588.sh
+++ d3588.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-ARCH=`uname -m`
+tool=$(which aarch64-linux-gnu-gcc)
+export CROSS_COMPILE_ARM64="${tool%gcc}"
+echo "using gcc: [${CROSS_COMPILE_ARM64}]"
 
-if [ X"${ARCH}" == X"x86_64" ] ; then
-	GCC=`realpath ../gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu`
-	CROSS_COMPILE_ARM64=${GCC}/bin/aarch64-none-linux-gnu-
-else
-	echo "${ARCH} is not supported now!"
-	exit 1
-fi
-
-# Clean the old files.
-rm -rf spl/u-boot-spl* #tpl/
+rm -rf spl/u-boot-spl*
 
 # Start building (U-Boot, SPL, etc.)
 make CROSS_COMPILE=${CROSS_COMPILE_ARM64} rk3588_defconfig
@@ -24,9 +17,7 @@ make CROSS_COMPILE=${CROSS_COMPILE_ARM64} -j`nproc`
 
 ls -alh fit/uboot.itb
 
-mkdir -p ../rockdev/
-cp -a fit/uboot.itb ../rockdev/uboot.img
-ls -alh ../rockdev/uboot.img
-md5sum ../rockdev/uboot.img
+cp -a fit/uboot.itb uboot.img
+ls -alh uboot.img
 echo "All done!"
 
